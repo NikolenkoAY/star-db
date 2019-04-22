122c425df6ea550f7446c78d3e5492599aab1b50
Alex <nikolenko.a.y@gmail.com>
2019-04-22 14:06:20 +0300

    1/2 lesson 65

--- src/component/item-list/item-list.js
+++ src/component/item-list/item-list.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
+import SwapiSevice from "../../services/swapi-services";
+
 import './item-list.css';
+import Spinner from '../spinner';
+
 
 export default class ItemList extends Component {
 
+  swapiSevice = new SwapiSevice();
+
+  state = {
+    peopleList: null,
+  }
+
+  componentDidMount(){
+    this.swapiSevice
+      .getAllPeople()
+      .then((peopleList)=>{
+        this.setState({
+          peopleList
+        });
+      })
+     
+  }
+
+  onError = (err)=>{
+    console.log("error" + err)
+  }
+
   render() {
+
+    const {peopleList}=this.state;
+
+    if (!peopleList){
+      return <Spinner/>
+    }
+
     return (
       <ul className="item-list list-group">
         <li className="list-group-item">
