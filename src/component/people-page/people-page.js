b52143a3f48f6db5675127e5c2372c235da820a3
Alexey Nikolenko <nikolenkoay@gmail.com>
2019-05-18 13:34:01 +0300

    lesson 74

--- src/component/people-page/people-page.js
+++ src/component/people-page/people-page.js
@@ -25,23 +25,29 @@ export default class PeoplePage extends Component {
     });
   };
   render() {
-    return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList
-            onPersonSelected={this.onPersonSelected}
-            getData={this.swapiService.getAllPeople}
-            renderItem={({ name, birthYear }) => `${name}-${birthYear}`}
-          />
-        </div>
-        <div className="col-md-6  ">
-          {this.state.hasError ? (
-            <ErrorIndicator />
-          ) : (
-            <PersonDetails personId={this.state.selectedPerson} />
-          )}
-        </div>
-      </div>
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
+
+    const itemList = (
+      <ItemList
+        onPersonSelected={this.onPersonSelected}
+        getData={this.swapiService.getAllPeople}
+        renderItem={({ name, birthYear }) => `${name}-${birthYear}`}
+      />
     );
+    const personDetails = (
+      <PersonDetails personId={this.state.selectedPerson} />
+    );
+    return <Row left={itemList} right={personDetails} />;
   }
 }
+
+const Row = ({ left, right }) => {
+  return (
+    <div className="row mb2">
+      <div className="col-md-6">{left}</div>
+      <div className="col-md-6  ">{right}</div>
+    </div>
+  );
+};
