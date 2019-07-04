edd9be7d69ea5dcab727ca2f90f5c49d4d90b99c
Alexey Nikolenko <nikolenkoay@gmail.com>
2019-07-04 22:48:17 +0300

    Relative paths - lesson 100

--- src/component/pages/starship-page.js
+++ src/component/pages/starship-page.js
@@ -1,41 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
+import { withRouter } from "react-router-dom";
 
-import Row from "../row";
-import ErrorBoundry from "../error-boundry";
-import {StarshipDetails, StarshipList } from "../sw-components";
- 
+import { StarshipList } from "../sw-components";
+
 import "../pages/pages.css";
 
-export default class StarshipPage extends Component {
-
-
-  state = {
-    selectedItem: null
-  };
-
-  onItemSelected = id => {
-    this.setState({
-      selectedItem: id
-    });
-  };
-
-  render() {
-      const {selectedItem} =this.state;
-    return (
-      <div>
-        <Row
-          left={
-            <ErrorBoundry>
-              <StarshipList onItemSelected={this.onItemSelected} />
-            </ErrorBoundry>
-          }
-          right={
-            <ErrorBoundry>
-              <StarshipDetails itemId={selectedItem} />
-            </ErrorBoundry>
-          }
-        />
-      </div>
-    );
-  }
-}
+const StarshipPage = ({ history }) => {
+  return <StarshipList onItemSelected={itemId => history.push(itemId)} />;
+};
+export default withRouter(StarshipPage);
